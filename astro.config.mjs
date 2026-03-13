161bfe585f67cae278cac838f660e6d499a32c64
Edward Meehan <ed@edmeehan.dev>
2026-03-13 02:26:14 -0700

    update basic styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com", // TODO: Update to your production URL
+  site: "https://edmeehan.dev", // TODO: Update to your production URL
   build: {
     inlineStylesheets: "always",
   },
